539f133547532c731c6a97299643a1f7828b9d7b
Albert Xie <phone.b.wind@gmail.com>
2018-10-31 17:44:22 +0800

    Including font as static files to fix google font api issue for microsoft edge

--- src/main/resources/static/js/coupon.js
+++ src/main/resources/static/js/coupon.js
@@ -3,23 +3,39 @@ HotelCoupon = window.HotelCoupon || {};
 HotelCoupon.CouponFn= function(){
     var coupon = this;
     coupon.$printPopupTriggerSel = ".print-btn.popup,.coupon-item";
+    coupon.$printPopupPreviewSel = ".print-btn.preview";
     coupon.$printPopupSel = ".coupon-item-detail.modal";
+    coupon.$printDetailSel = ".coupon-item-detail";
+    coupon.$contentSel = ".coupon.content";
     coupon.$printPopupAttr = "value";
+    coupon.$modalHidden = "hidden";
+    coupon.getPopupDetail = function($coupon){
+        var couponId = $coupon.attr(coupon.$printPopupAttr);
+        var $targetCouponPopup = $(coupon.$printPopupSel).filter(function(){
+            return $(this).attr(coupon.$printPopupAttr)===couponId;
+        });
+        return $targetCouponPopup;
+    }
     coupon.popupDetail = function($couponPopup){
-        if($couponPopup.is(":visible")){
-            $couponPopup.hide();
-        }else{
-            $couponPopup.show()
-        }
+        $(coupon.$printDetailSel).addClass(coupon.$modalHidden);
+        $couponPopup.removeClass(coupon.$modalHidden);
+    };
+    coupon.print = function($couponPopup){
+        $(".coupon.content").append($couponPopup.clone());
+        setTimeout(function(){
+            window.print();
+            $(coupon.$contentSel).children().last().remove();
+        });
     };
     coupon.init = function(){
         $(coupon.$printPopupTriggerSel).click(function(){
-            var couponId = $(this).attr(coupon.$printPopupAttr);
-            var $targetCouponPopup = $(coupon.$printPopupSel).filter(function(){
-                return $(this).attr(coupon.$printPopupAttr)===couponId;
-            });
+            var $targetCouponPopup = coupon.getPopupDetail($(this));
             coupon.popupDetail($targetCouponPopup);
             return false;
+        });
+        $(coupon.$printPopupPreviewSel).click(function(){
+            var $targetCouponPopup = coupon.getPopupDetail($(this));
+            coupon.print($targetCouponPopup);
         });
     };
 };
@@ -27,3 +43,4 @@ $(function(){
     var coupon = new HotelCoupon.CouponFn();
     coupon.init();
 });
+
